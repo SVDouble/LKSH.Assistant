0329b5b79a70e329e0eccbe9140985d46296b3ff
Chrn1y <artyom.sushkov@gmail.com>
2018-07-06 16:35:50 +0300

    Initial commit

---
+++ app/src/main/java/com/lksh/dev/lkshassistant/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lksh.dev.lkshassistant
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
