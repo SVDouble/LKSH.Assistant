3b96f7b811f96c824cffe221f01b3c3582ddedaa
ky3he4ik <misha10311@yandex.ru>
2018-07-07 06:08:45 +0300

    showing empty map

--- app/src/main/java/com/lksh/dev/lkshassistant/MainActivity.kt
+++ app/src/main/java/com/lksh/dev/lkshassistant/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.lksh.dev.lkshassistant
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, MapActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/lksh/dev/lkshassistant/MapInterface.kt
@@ -0,0 +1,48 @@
+package com.lksh.dev.lkshassistant
+
+import android.app.Activity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import kotlinx.android.synthetic.main.map_layout.*
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.XYTileSource
+
+class MapActivity : Activity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //handle permissions first, before map is created. not depicted here
+
+        //load/initialize the osmdroid configuration, this can be done
+        val ctx = applicationContext
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
+        //setting this before the layout is inflated is a good idea
+        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
+        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
+        //see also StorageUtils
+        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
+
+        //XYTileSource("0", 0, 17, 256, ".png", Array(0){""})
+
+        //inflate and create the map
+        setContentView(R.layout.map_layout)
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map!!.onResume() //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map!!.onPause()  //needed for compass, my location overlays, v6.0.0 and up
+    }
+}
