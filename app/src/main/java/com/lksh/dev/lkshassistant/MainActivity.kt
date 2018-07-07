3a7cf5ee64429186f0e23771e6752940ad84ef3e
SVDouble <hpp@tuta.io>
2018-07-07 16:38:38 +0300

    Implement bottom navigation and swiping

--- app/src/main/java/com/lksh/dev/lkshassistant/MainActivity.kt
+++ app/src/main/java/com/lksh/dev/lkshassistant/MainActivity.kt
@@ -1,12 +1,71 @@
 package com.lksh.dev.lkshassistant
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
+import android.support.design.widget.BottomNavigationView
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ProfileFragment.OnFragmentInteractionListener {
+
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
+
+    private val mOnNavigationItemSelectedListener
+            = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                map.setCurrentItem(0, true)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                map.setCurrentItem(1, true)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                map.setCurrentItem(2, true)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+        map.adapter = mSectionsPagerAdapter
+        map.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+            override fun onPageSelected(position: Int) {
+                navigation.selectedItemId = when (position) {
+                    0 -> R.id.navigation_home
+                    1 -> R.id.navigation_dashboard
+                    2 -> R.id.navigation_notifications
+                    else -> throw IndexOutOfBoundsException("Wrong!!!")
+                }
+            }
+        })
+    }
+
+    class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            return ProfileFragment()
+        }
+
+        override fun getCount() = 3
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+
     }
 }
