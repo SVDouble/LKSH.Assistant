b6083255d12608810f1a017181acebe533de244f
ky3he4ik <misha10311@yandex.ru>
2018-07-08 12:10:02 +0300

    showing map at min scale

--- app/src/main/java/com/lksh/dev/lkshassistant/MainActivity.kt
+++ app/src/main/java/com/lksh/dev/lkshassistant/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.lksh.dev.lkshassistant
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        Log.e("START", "I'm started")
         setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/lksh/dev/lkshassistant/MapFragment.kt
+++ app/src/main/java/com/lksh/dev/lkshassistant/MapFragment.kt
@@ -1,16 +1,21 @@
 package com.lksh.dev.lkshassistant
 
+import android.Manifest
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.mapsforge.core.graphics.Filter
+import org.mapsforge.core.model.BoundingBox
 import org.mapsforge.core.model.LatLong
+import org.mapsforge.core.model.MapPosition
+import org.mapsforge.core.util.LatLongUtils
 import org.mapsforge.map.android.graphics.AndroidGraphicFactory
 import org.mapsforge.map.android.util.AndroidUtil
 import org.mapsforge.map.android.view.MapView
@@ -24,6 +29,10 @@ import java.io.FileNotFoundException
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_LATITUDE = "lat"
 private const val ARG_LONGITUDE = "long"
+private const val minLat = 57.855300
+private const val maxLat = 57.858790
+private const val minLong = 41.708843
+private const val maxLong = 41.717549
 
 /**
  * A simple [Fragment] subclass.
@@ -38,8 +47,27 @@ class MapFragment : Fragment() {
     private var listener: OnFragmentInteractionListener? = null
     private var mapView: MapView? = null
 
+    /*
+    String fileList[] = am.list(path);
+
+            for (String fileName : fileList) {
+
+                // open file within the assets folder
+                // if it is not already there copy it to the sdcard
+                String pathToDataFile = DATA_PATH + path + "/" + fileName;
+                if (!(new File(pathToDataFile)).exists()) {
+                }
+            }
+     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        //TODO: request permissions: write files and gps
         super.onCreate(savedInstanceState)
+        Log.i("START", "I'm started")
+        Log.i("MAP", "map fragment started")
+        for (it in activity!!.assets.list("."))
+            Log.i("ASSET_PATH", it)
+
         AndroidGraphicFactory.createInstance(activity!!.application)
         try {
             mapView = MapView(context)
@@ -50,10 +78,18 @@ class MapFragment : Fragment() {
             val tileCache = AndroidUtil.createTileCache(context, "mapcache",
                     mapView!!.model.displayModel.tileSize, 1f,
                     mapView!!.model.frameBufferModel.overdrawFactor)
-            val mapFile = File(Environment.getExternalStorageDirectory(), "map.map")
+            val mapFolder = File(Environment.getExternalStorageDirectory(), "lksh")
+            if (!mapFolder.exists())
+                mapFolder.mkdir()
+            val mapFile = File(mapFolder, "map.map")
             if (!mapFile.exists()) {
                 mapFile.createNewFile()
-                throw (FileNotFoundException(Environment.getExternalStorageDirectory().absolutePath + " map.map"))
+                activity!!.assets.open("map.map").copyTo(mapFile.outputStream())
+                Log.i("MAP", "copy from assets to ${mapFile.absolutePath}")
+
+                //throw (FileNotFoundException(Environment.getExternalStorageDirectory().absolutePath + "map.map"))
+            } else {
+                Log.i("MAP", "${mapFile.absolutePath} already exists")
             }
             val mapDataStore = MapFile(mapFile)
             val tileRendererLayer = TileRendererLayer(tileCache, mapDataStore,
@@ -62,11 +98,52 @@ class MapFragment : Fragment() {
 
             mapView!!.layerManager.layers.add(tileRendererLayer)
 
-            mapView!!.setCenter(LatLong(57.51457, 41.42603))
+            mapView!!.mapZoomControls.zoomLevelMax = 3
+            mapView!!.mapZoomControls.zoomLevelMin = 1
             mapView!!.setZoomLevel(12.toByte())
+            mapView!!.mapZoomControls.isShowMapZoomControls = true
+            mapView!!.setBuiltInZoomControls(true)
+
+            mapView!!.setCenter(LatLong(Bundle().getDouble(ARG_LATITUDE), Bundle().getDouble(ARG_LATITUDE)))
+            activity!!.findViewById<MapView>(R.id.mapView).mapZoomControls.zoomLevelMax = 1
+            val boundingBox = BoundingBox(minLat, minLong, maxLat, maxLong)
+            val dimension = mapView!!.model.mapViewDimension.dimension
+            mapView!!.model.mapViewPosition.setMapPosition(MapPosition(boundingBox.centerPoint,
+                    LatLongUtils.zoomForBounds(dimension, boundingBox,
+                            mapView!!.model.displayModel.tileSize)))
+            /*
+            @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            BoundingBox bb = new BoundingBox(latLong2.latitude,
+                    latLong3.longitude, latLong3.latitude, latLong2.longitude);
+            Dimension dimension = this.mapView.getModel().mapViewDimension.getDimension();
+            this.mapView.getModel().mapViewPosition.setMapPosition(new MapPosition(
+                    bb.getCenterPoint(),
+                    LatLongUtils.zoomForBounds(
+                            dimension,
+                            bb,
+                            this.mapView.getModel().displayModel.getTileSize())));
+        }
+    }
+
+    @Override
+    protected void addOverlayLayers(Layers layers) {
+        Polyline polyline = new Polyline(Utils.createPaint(
+                AndroidGraphicFactory.INSTANCE.createColor(Color.BLUE), 8,
+                Style.STROKE), AndroidGraphicFactory.INSTANCE);
+        List<LatLong> latLongs = new ArrayList<>();
+        latLongs.add(latLong2);
+        latLongs.add(latLong3);
+        polyline.setPoints(latLongs);
+        layers.add(polyline);
+    }
+
+             */
 
         } catch (e: Exception) {
-            Log.e("LKSH-MAP", e.message, e)
+            Log.e("LKSH_MAP", e.message, e)
         }
     }
 
@@ -84,11 +161,11 @@ class MapFragment : Fragment() {
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
+        /*if (context is OnFragmentInteractionListener) {
             listener = context
         } else {
             throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
+        }*/
     }
 
     override fun onDetach() {
@@ -135,17 +212,6 @@ class MapFragment : Fragment() {
                     }
                 }
     }
-
-    /*
-
-    public static MapaForgeFragment newInstance() {
-        MapaForgeFragment fragment = new MapaForgeFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
-     */
-
 }
 //57.85722%2C41.71562#map=18/57.85760/41.70948
 //geoURI: geo://57.85760,41.70948?z=18
