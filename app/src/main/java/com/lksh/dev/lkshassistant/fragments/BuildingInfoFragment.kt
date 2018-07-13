66f23e61f288f28855d9e9f3f82b0f0ce2e886a9
Nikita <nime@ya.ru>
2018-07-13 17:07:40 +0300

    add rules

--- app/src/main/java/com/lksh/dev/lkshassistant/fragments/BuildingInfoFragment.kt
+++ app/src/main/java/com/lksh/dev/lkshassistant/fragments/BuildingInfoFragment.kt
@@ -14,10 +14,12 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.lksh.dev.lkshassistant.R
+import com.lksh.dev.lkshassistant.R.id.*
 import com.lksh.dev.lkshassistant.R.raw.test
 import com.lksh.dev.lkshassistant.activities.MainActivity
 import com.lksh.dev.lkshassistant.sqlite_helper.DBWrapper
 import com.lksh.dev.lkshassistant.utils.ContactsData
+import com.lksh.dev.lkshassistant.utils.RulesLkshData
 import kotlinx.android.synthetic.main.fragment_building_info.*
 import kotlinx.android.synthetic.main.part_rv_building.view.*
 import org.w3c.dom.Text
@@ -50,6 +52,22 @@ class BuildingInfoFragment : Fragment() {
             contacts.findViewById<LinearLayout>(item.handlerView).findViewById<TextView>(R.id.phone).text = item.phone
             contacts.findViewById<LinearLayout>(item.handlerView).findViewById<ImageView>(R.id.userPhoto).setImageDrawable(resources.getDrawable(item.imageSrc, resources.newTheme()))
         }
+
+        goodRules.findViewById<TextView>(R.id.goodRulesHeader).text = "Правила"
+
+        var rulesCards = listOf(R.id.goodRule1, R.id.goodRule2, R.id.goodRule3, R.id.goodRule4, R.id.goodRule5, R.id.goodRule6, R.id.goodRule7, R.id.goodRule8, R.id.goodRule9, R.id.goodRule10, R.id.goodRule11, R.id.goodRule12, R.id.goodRule13, R.id.goodRule14, R.id.goodRule15)
+        var curIndex = 0
+
+        for (rule in RulesLkshData.rules){
+            var emojiPositiveRate = "\uD83D\uDC4E"
+            if (rule.isPositive){
+                emojiPositiveRate = "\uD83D\uDC4D"
+            }
+            goodRules.findViewById<LinearLayout>(rulesCards[curIndex]).findViewById<TextView>(R.id.mark).text = emojiPositiveRate
+            goodRules.findViewById<LinearLayout>(rulesCards[curIndex]).findViewById<TextView>(R.id.text).text = rule.text
+            curIndex++
+        }
+
     }
 
     override fun onStart() {
