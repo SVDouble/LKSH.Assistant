4c1e615811e9ebebc6a5860ef5f70fdd2ec66954
slava <slava.pach@gmail.com>
2018-07-13 16:24:46 +0300

    add contacts

--- app/src/main/java/com/lksh/dev/lkshassistant/fragments/BuildingInfoFragment.kt
+++ app/src/main/java/com/lksh/dev/lkshassistant/fragments/BuildingInfoFragment.kt
@@ -1,16 +1,23 @@
 package com.lksh.dev.lkshassistant.fragments
 
+import android.content.res.Resources
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.media.Image
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.lksh.dev.lkshassistant.R
+import com.lksh.dev.lkshassistant.R.raw.test
 import com.lksh.dev.lkshassistant.activities.MainActivity
 import com.lksh.dev.lkshassistant.sqlite_helper.DBWrapper
+import com.lksh.dev.lkshassistant.utils.ContactsData
 import kotlinx.android.synthetic.main.fragment_building_info.*
 import kotlinx.android.synthetic.main.part_rv_building.view.*
 import org.w3c.dom.Text
@@ -36,9 +43,13 @@ class BuildingInfoFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        val block = listOf(contacts)
-
         contacts.findViewById<TextView>(R.id.header).text = "Контакты"
+        for (item in ContactsData.contacts) {
+            contacts.findViewById<LinearLayout>(item.handlerView).findViewById<TextView>(R.id.name).text = item.name
+            contacts.findViewById<LinearLayout>(item.handlerView).findViewById<TextView>(R.id.role).text = item.role
+            contacts.findViewById<LinearLayout>(item.handlerView).findViewById<TextView>(R.id.phone).text = item.phone
+            contacts.findViewById<LinearLayout>(item.handlerView).findViewById<ImageView>(R.id.userPhoto).setImageDrawable(resources.getDrawable(item.imageSrc, resources.newTheme()))
+        }
     }
 
     override fun onStart() {
