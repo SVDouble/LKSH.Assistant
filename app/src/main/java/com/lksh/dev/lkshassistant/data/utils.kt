3493a7dd5cf375f78d99d6b595c9c625d840f40c
svdouble <hpp@tuta.io>
2018-07-20 15:06:43 +0300

    Update csv parser to correctly handle commas

--- app/src/main/java/com/lksh/dev/lkshassistant/data/utils.kt
+++ app/src/main/java/com/lksh/dev/lkshassistant/data/utils.kt
@@ -7,6 +7,8 @@ import java.io.InputStreamReader
 fun parseCsv(ctx: Context, resourceId: Int, delimiter: String = ","): List<List<String>> {
     val inputStream = ctx.resources.openRawResource(resourceId)
     return BufferedReader(InputStreamReader(inputStream)).readLines().map {
-        it.split(delimiter)
+        it.split(delimiter).map {
+            it.trim(' ').replace('^', ',')
+        }
     }
 }
