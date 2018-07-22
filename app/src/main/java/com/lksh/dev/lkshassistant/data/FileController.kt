be6360b47e3dffa9e67ec97e18166fe32db1cdb8
svdouble <hpp@tuta.io>
2018-07-22 17:24:42 +0300

    Update FileController prototype

--- app/src/main/java/com/lksh/dev/lkshassistant/data/FileController.kt
+++ app/src/main/java/com/lksh/dev/lkshassistant/data/FileController.kt
@@ -1,6 +1,9 @@
 package com.lksh.dev.lkshassistant.data
 
 import android.content.Context
+import com.beust.klaxon.Klaxon
+
+const val FC_CONFIG_FILENAME = "files_config.json"
 
 class FileController private constructor() {
     companion object {
@@ -8,23 +11,39 @@ class FileController private constructor() {
 
         @JvmStatic
         fun getFile(ctx: Context, fileName: String): String {
-            return ""
+            fetchVersions(ctx)
+            if ((localVersions[fileName] ?: -1) < serverVersions[fileName]!!)
+                updateFile(ctx, fileName)
+            return readFromFS(ctx, fileName)!!
         }
 
 
         /* Inner logic */
 
         /* Key: filename; Value: version */
-        val versions: Map<String, Int> = mapOf()
+        private var localVersions: MutableMap<String, Int> = mutableMapOf()
+        private var serverVersions: MutableMap<String, Int> = mutableMapOf()
 
         @JvmStatic
         private fun updateFile(ctx: Context, fileName: String) {
-
+            val newValue = "" //NetworkHelper. ...
+            writeToFS(ctx, fileName, newValue)
         }
 
         @JvmStatic
-        private fun fetchVersions(ctx: Context, fileName: String) {
+        private fun fetchVersions(ctx: Context) {
+            /* From server */
 
+            /* From FS */
+            localVersions.clear()
+            Klaxon().parseArray<FileInfo>(readFromFS(ctx, FC_CONFIG_FILENAME)!!)!!
+                    .map { it.fileName to it.fileVersion }
+                    .toMap(localVersions)
         }
     }
+
+    private data class FileInfo(
+            val fileName: String,
+            val fileVersion: Int
+    )
 }
