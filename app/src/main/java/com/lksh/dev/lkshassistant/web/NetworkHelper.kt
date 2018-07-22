33db9c32a026b026560da9be13be0fa850c9f1e8
Vladislav Annenkov <zzoorm@gmail.com>
2018-07-22 16:06:01 +0300

    Refactor auth

--- app/src/main/java/com/lksh/dev/lkshassistant/web/NetworkHelper.kt
+++ app/src/main/java/com/lksh/dev/lkshassistant/web/NetworkHelper.kt
@@ -6,16 +6,46 @@ import com.github.kittinunf.fuel.core.Response
 import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
 import com.lksh.dev.lkshassistant.AppSettings
+import org.json.JSONObject
+import java.net.UnknownHostException
 
 typealias httpResponse = (Request, Response, Result<String, FuelError>) -> Unit
 
 class NetworkHelper private constructor() {
     companion object {
         @JvmStatic
-        fun authUser(login: String, password: String, handler: httpResponse) {
+        fun authUser(login: String, password: String) {
             val authUrl = AppSettings.baseUrl + "/user_auth/"
-            authUrl.httpPost(listOf(Pair("login", login),
-                    Pair("password", password))).timeout(5000).responseString(Charsets.UTF_8, handler)
+            authUrl.httpPost(listOf(Pair("login", login), Pair("password", password)))
+                    .timeout(5000).responseString { request, response, result ->
+                        var token: String? = null
+                        var responseState: Auth.ResponseState
+
+                        when (result) {
+                            is Result.Success -> {
+                                try {
+                                    token = JSONObject(result.get())
+                                            .getJSONArray("result")
+                                            .getJSONObject(0)
+                                            .getString("token")
+                                } catch (e: UnknownHostException) {
+                                    token = null
+                                    responseState = Auth.ResponseState.SERVER_NOT_FOUND
+                                }
+                            }
+                            is Result.Failure -> {
+                                responseState = Auth.ResponseState.TIMEOUT_REACHED
+                            }
+                        }
+                    }
+        }
+
+        @JvmStatic
+        fun getUsers(token: String, handler: httpResponse) {
+            val authUrl = AppSettings.baseUrl + "/get_users/"
+            authUrl.httpPost(listOf(Pair("token", authUrl)))
+                    .timeout(5000).responseString { request, response, result ->
+                    }
         }
     }
 }
