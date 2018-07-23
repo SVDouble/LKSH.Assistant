8475bd7f04b53609a0e6a8674962c3f380c18898
Daniil Leonov <leonowdanya@yandex.ru>
2018-07-23 10:15:43 +0300

    Work upload of data

--- app/src/main/java/com/lksh/dev/lkshassistant/web/JsoupHtml.kt
+++ app/src/main/java/com/lksh/dev/lkshassistant/web/JsoupHtml.kt
@@ -1,30 +1,26 @@
 package com.lksh.dev.lkshassistant.web
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
-import com.beust.klaxon.JsonArray
-import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Klaxon
 import com.lksh.dev.lkshassistant.data.Prefs
 import com.lksh.dev.lkshassistant.data.SingletonHolder
 import org.jetbrains.anko.runOnUiThread
-import org.jetbrains.anko.toast
-import org.json.JSONArray
 import org.jsoup.Jsoup
 
+data class Schedule(var error : String, var result : List<ScheduleItem>)
 data class ScheduleItem(var time : String = "", var description : String = "")
 
 class JsoupHtml(val ctx: Context) {
 
     private val TIMEOUT = 5000
-    private val URL_REQUEST = "assistant.p2.lksh.ru/get_timetable_events/"/* "http://ejudge.lksh.ru/"*/
+    private val URL_REQUEST = "http://assistant.p2.lksh.ru/get_timetable_events"
 
     fun parseHtml() {
         var timetable = ""
-        Jsoup.connect(URL_REQUEST).timeout(TIMEOUT).get().body().run {
-            val items = Klaxon().parseArray<ScheduleItem>(text().replace("'", "\"")) ?: JsonArray()
-            for (i in items)
+        Jsoup.connect(URL_REQUEST).timeout(TIMEOUT).ignoreContentType(true).get().body().run {
+            val it = Klaxon().parse<Schedule>(text())
+            for (i in it?.result ?: listOf())
                 timetable += i.time + " " + i.description + '\n'
             ctx.runOnUiThread {
                 Prefs.getInstance(ctx).timetable = timetable
